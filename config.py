c70f590e76231cabc0207ae74938a1e5d33c0d32
Hub972 <hassouvie@gmail.com>
2019-05-09 18:15:42 +0200

    first template

---
+++ config.py
@@ -0,0 +1,4 @@
+# To generate a new secret key:
+# >>> import random, string
+# >>> "".join([random.choice(string.printable) for _ in range(24)])
+SECRET_KEY = "!&hjeVc=S'V\o{$0'_B#E b?<"
--- grand_py/views.py
+++ grand_py/views.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
+# Config options - Make sure you created a 'config.py' file.
+app.config.from_object('config')
+# To get one variable, tape app.config['MY_VARIABLE']
+
 
 @app.route('/')
+@app.route('/mainPage/')
 def mainPage():
-    return "Hello world"
+
+    return render_template('mainPage.html', picture = url_for('static', filename='img/papy4.png'))
