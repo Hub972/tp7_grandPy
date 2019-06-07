50848570ca7a4202fc59028c4abe844674416550
Hub972 <hassouvie@gmail.com>
2019-06-07 18:02:56 +0200

    Add requirements.

--- grand_py/wiki_pack/wikipy.py
+++ grand_py/wiki_pack/wikipy.py
@@ -6,7 +6,7 @@ import wikipedia
 class Wiki:
 
     def searchContent(self, query):
-        """Search a page about a query and select a text"""
+        """Search a page about a query and select a text for alert"""
         wikipedia.set_lang("fr")
         resultRaw = wikipedia.page(query)
         resultList = resultRaw.content[0:295]
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
+# coding: utf-8
 from grand_py import app
 
 if __name__ == "__main__":
